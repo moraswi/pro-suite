5b753d4300667e23c5033ae3c55f54ee4cdf58df
moraswi <phineas@softioc.co.za>
2024-03-10 22:57:01 +0200

    format code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-// import React, { useState } from 'react';
 import { useRef } from "react";
 import '../styles/NavBar.css'
 import logo from "../assets/logo.png" 
@@ -6,25 +5,21 @@ import logo from "../assets/logo.png"
 function Navbar() {
 	const navRef = useRef();
 
-
 	return (
 		<header>
             <div className="mainNavBarContainer">
-		
             <img
-        src={logo}
-        alt="logo"
-        className="logo"
-      />
+                src={logo}
+                alt="logo"
+                className="logo"
+            />
 			<nav ref={navRef}>
 				<a className="aLink" href="/#home">HOME</a>
 				<a className="aLink" href="/#about-us">ABOUT</a>
 				<a className="aLink" href="/#solution">SOLUTION</a>
 				<a className="aLink" href="/#contact-us">CONTACT</a>
                 <button className='requestDemoNav'>REQUEST A DEMO</button>
-
 			</nav>
-
             </div>
 		</header>
 	);
